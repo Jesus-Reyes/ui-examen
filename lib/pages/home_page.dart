2517ab1bd35a142d67b0bee01f4d039c500ba13c
Jesus Reyes <chuchin43@outlook.com>
2022-04-18 18:01:42 -0500

    UI orientacion agregada, con los contenedores

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-class HomePage extends StatelessWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-const HomePage({Key? key}) : super(key: key);
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final colors = [Colors.blue, Colors.red, Colors.green, Colors.yellow];
+    final containers = List.generate(4, (index) => Container(color: colors[index], height: size.height * .25));
 
-    return  Scaffold(
-      body: SizedBox(
-        width: size.width,
-        child: const Text('Hola Mundo'),
-     ),
-   );
+    return Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return SizedBox(
+          height: 1200,
+          width: size.width,
+          child: Column(
+            children: containers,
+          ),
+        );
+        },
+      ),
+    );
   }
 }
